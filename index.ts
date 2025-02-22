509958f90f2555aba285774337c6178a86c7d355
Derek van Vliet <derek@bikesky.social>
2025-02-22 09:22:12 -0500

    added try/catches around the query event fetch, profile fetches and webhook send

--- index.ts
+++ index.ts
@@ -12,7 +12,6 @@ async function ozoneDiscordPoster() {
   const ozoneUrl = process.env.OZONE_URL as string;
 
   const webhook = webhookUrl ? new Webhook(webhookUrl) : undefined;
-
   const agent = new AtpAgent({ service: "https://bsky.social" });
 
   try {
@@ -29,66 +28,6 @@ async function ozoneDiscordPoster() {
   let first = true;
   let lastId = 0;
 
-  async function postWebhook(event: ModEventView, isReport: boolean) {
-    console.log(`posting: ${JSON.stringify(event)}`);
-
-    const message = new MessageBuilder();
-
-    const creatorProfile = await agent.getProfile({ actor: event.createdBy });
-
-    message.setAuthor(
-      event.creatorHandle,
-      creatorProfile.data.avatar,
-      `https://bsky.app/profile/${event.createdBy}`
-    );
-
-    message.setURL(
-      `${ozoneUrl}/reports?quickOpen=${encodeURIComponent(
-        event.subject.uri ?? event.subject.did ?? ""
-      )}`
-    );
-
-    if (event.subject.$type === "com.atproto.admin.defs#repoRef") {
-      // account
-      message.setColor("#9B59B6");
-      const subjectProfile = await agent.getProfile({
-        actor: event.subject.did,
-      });
-      message.setTitle(`account: ${event.subjectHandle}`);
-      message.addField(
-        "Account bio:",
-        subjectProfile.data.description ? subjectProfile.data.description : ""
-      );
-    } else if (event.subject.$type === "com.atproto.repo.strongRef") {
-      if (event.subject.uri.includes("app.bsky.feed.post")) {
-        // post
-        message.setColor("#3498DB");
-        message.setTitle(`post by ${event.subjectHandle}`);
-        const posts = await agent.getPosts({ uris: [event.subject.uri] });
-        if (posts.data.posts.length > 0) {
-          message.addField(
-            "Post:",
-            posts.data.posts[0].record.text
-              ? posts.data.posts[0].record.text
-              : ""
-          );
-        }
-      } else if (event.subject.uri.includes("app.bsky.graph.list")) {
-        // list
-        message.setColor("#2ECC71");
-        message.setTitle(`list by ${event.subjectHandle}`);
-      }
-    }
-
-    message.addField(
-      "Comment:",
-      event.event.comment ? event.event.comment : ""
-    );
-    message.setTimestamp();
-
-    webhook.send(message);
-  }
-
   Bun.serve({
     fetch(req) {
       const url = new URL(req.url);
@@ -100,32 +39,105 @@ async function ozoneDiscordPoster() {
   console.log("server is listening");
 
   while (true) {
-    const response = await agent.tools.ozone.moderation.queryEvents(
-      {},
-      {
-        headers: {
-          "atproto-proxy": `${bskyLabelerDid}#atproto_labeler`,
-        },
-      }
-    );
+    try {
+      const response = await agent.tools.ozone.moderation.queryEvents(
+        {},
+        {
+          headers: {
+            "atproto-proxy": `${bskyLabelerDid}#atproto_labeler`,
+          },
+        }
+      );
 
-    const events = response.data.events;
+      const events = response.data.events;
 
-    for (let i = 0; i < events.length; i++) {
-      const event = events[i];
-      if (event.event.$type !== "tools.ozone.moderation.defs#modEventReport")
-        continue;
-      if (first || lastId == event.id) {
-        first = false;
+      for (let i = 0; i < events.length; i++) {
+        const event = events[i];
+        if (event.event.$type !== "tools.ozone.moderation.defs#modEventReport")
+          continue;
+        if (first || lastId == event.id) {
+          first = false;
+          lastId = event.id;
+          break;
+        }
         lastId = event.id;
+        await postWebhook(event, true);
         break;
       }
-      lastId = event.id;
-      await postWebhook(event, true);
-      break;
+    } catch (error) {
+      console.log(
+        `failed to query moderation events: ${JSON.stringify(error)}`
+      );
     }
 
     await sleep(pollingSeconds * 1000);
+  }
+
+  async function postWebhook(event: ModEventView, isReport: boolean) {
+    console.log(`posting: ${JSON.stringify(event)}`);
+
+    const message = new MessageBuilder();
+
+    try {
+      const creatorProfile = await agent.getProfile({ actor: event.createdBy });
+
+      message.setAuthor(
+        event.creatorHandle,
+        creatorProfile.data.avatar,
+        `https://bsky.app/profile/${event.createdBy}`
+      );
+
+      message.setURL(
+        `${ozoneUrl}/reports?quickOpen=${encodeURIComponent(
+          event.subject.uri ?? event.subject.did ?? ""
+        )}`
+      );
+
+      if (event.subject.$type === "com.atproto.admin.defs#repoRef") {
+        // account
+        message.setColor("#9B59B6");
+        const subjectProfile = await agent.getProfile({
+          actor: event.subject.did,
+        });
+        message.setTitle(`account: ${event.subjectHandle}`);
+        message.addField(
+          "Account bio:",
+          subjectProfile.data.description ? subjectProfile.data.description : ""
+        );
+      } else if (event.subject.$type === "com.atproto.repo.strongRef") {
+        if (event.subject.uri.includes("app.bsky.feed.post")) {
+          // post
+          message.setColor("#3498DB");
+          message.setTitle(`post by ${event.subjectHandle}`);
+          const posts = await agent.getPosts({ uris: [event.subject.uri] });
+          if (posts.data.posts.length > 0) {
+            message.addField(
+              "Post:",
+              posts.data.posts[0].record.text
+                ? posts.data.posts[0].record.text
+                : ""
+            );
+          }
+        } else if (event.subject.uri.includes("app.bsky.graph.list")) {
+          // list
+          message.setColor("#2ECC71");
+          message.setTitle(`list by ${event.subjectHandle}`);
+        }
+      }
+
+      message.addField(
+        "Comment:",
+        event.event.comment ? event.event.comment : ""
+      );
+      message.setTimestamp();
+      try {
+        webhook.send(message);
+      } catch (error) {
+        console.log(`failed to send to webhook: ${JSON.stringify(error)}`);
+      }
+    } catch (error) {
+      console.log(`failed to fetch creator profile: ${JSON.stringify(error)}`);
+    }
   }
 }
 
